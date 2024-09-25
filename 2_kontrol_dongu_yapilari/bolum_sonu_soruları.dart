91f01c32d5269881b7e3f7e91ef26b4b43bf87cc
bugraygul <bugra06103@gmail.com>
2024-09-25 16:17:16 +0300

    3. sorunun cevabı

--- 2_kontrol_dongu_yapilari/bolum_sonu_soruları.dart
+++ 2_kontrol_dongu_yapilari/bolum_sonu_soruları.dart
@@ -6,16 +6,31 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  //Cevap 1
-  print("birinci sayiyi giriniz");
-  int birinciSayi = int.parse(stdin.readLineSync()!);
+  //Cevap 2------------------------------------
+  print("Vize notunuzu giriniz");
+  int vizeNotu = int.parse(stdin.readLineSync()!);
 
-  print("ikinci sayiyi giriniz");
-  int ikinciSayi = int.parse(stdin.readLineSync()!);
+  print("Final notunuzu giriniz");
+  int finalNotu = int.parse(stdin.readLineSync()!);
 
-  print("üçüncü sayiyi giriniz");
-  int ucuncuSayi = int.parse(stdin.readLineSync()!);
+  double notOrtalama = (vizeNotu * 40 + finalNotu * 60) /100;
+  print("Not ortalamanız $notOrtalama");
 
-  double ortalama = (birinciSayi + ikinciSayi + ucuncuSayi) / 3;
-  print("girdiğiniz $birinciSayi, $ikinciSayi ve $ucuncuSayi sayılarının ortalaması $ortalama");
+  if(notOrtalama >= 50){
+    print("Tebrikler not oratalamanız $notOrtalama ve dersten geçtiniz");
+  }else{
+    print("Üzgünüz not ortalamanız $notOrtalama ve dersten geçemediniz");
+  }
+  //Cevap 1-------------------------------------
+  // print("birinci sayiyi giriniz");
+  // int birinciSayi = int.parse(stdin.readLineSync()!);
+
+  // print("ikinci sayiyi giriniz");
+  // int ikinciSayi = int.parse(stdin.readLineSync()!);
+
+  // print("üçüncü sayiyi giriniz");
+  // int ucuncuSayi = int.parse(stdin.readLineSync()!);
+
+  // double ortalama = (birinciSayi + ikinciSayi + ucuncuSayi) / 3;
+  // print("girdiğiniz $birinciSayi, $ikinciSayi ve $ucuncuSayi sayılarının ortalaması $ortalama");
 }
